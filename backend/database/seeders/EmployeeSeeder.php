56d4c62ebf741fb52744433b2c764a09867a7f0e
JnthnPrz <jo.perez@mlgcl.edu.ph>
2025-03-27 17:13:35 +0800

    Activity 6 Note: Contenuation

--- backend/database/seeders/EmployeeSeeder.php
+++ backend/database/seeders/EmployeeSeeder.php
@@ -8,9 +8,7 @@ use App\Models\Employee;
 
 class EmployeeSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    
             public function run(): void
         {
             Employee::factory(10)->create(); 
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\AuthController;
 
-// Public Routes
+
 Route::post('/login', [AuthController::class, 'login']); 
 Route::get('/users', [AuthController::class, 'index']);
 Route::post('/logout', [AuthController::class, 'logout']);
@@ -13,7 +13,7 @@ Route::get('/employees', [EmployeeController::class, 'index']);
 Route::get('/students', [StudentController::class, 'index']);
 
 
-// Protected Routes
+
 Route::middleware('permission')->group(function () {
     Route::post('/employees', [EmployeeController::class, 'create']);
     Route::put('/employees/{id}', [EmployeeController::class, 'update']);
